cb8aa1e32cde04012af1ed91378e4c86477fbbc5
kuznetsovmaxim <kuzel1@rambler.ru>
2016-03-17 18:41:09 +0300

    Initial Commit

---
+++ DropIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropIt
+//
+//  Created by Максим Кузнецов on 17.03.16.
+//  Copyright © 2016 Kuznetsovmaxim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
